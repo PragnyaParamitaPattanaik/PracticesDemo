df303d9e830ac4532f55e6f20300d4891578d26a
PragnyaParamitaPattanaik <pragnya.pattanaik@gmail.com>
2022-06-13 20:01:21 -0700

    Commit

---
+++ src/PracticeDemo90/practice200.java
@@ -0,0 +1,19 @@
+package PracticeDemo90;
+
+import java.util.Scanner;
+
+public class practice200 {
+
+
+        public static void main(String[] args) {
+            Scanner in = new Scanner(System.in);
+
+            String S = in.next();
+            int start = in.nextInt();
+            int end = in.nextInt();
+            in.close();
+
+            System.out.println(S.substring(start, end));
+        }
+    }
+
