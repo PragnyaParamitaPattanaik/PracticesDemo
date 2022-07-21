b5eafccde8a8c2b33901f1828bf0c8b978667a13
PragnyaParamitaPattanaik <pragnya.pattanaik@gmail.com>
2022-07-20 19:12:22 -0700

    Commit

---
+++ src/ProgramDemo10/Stack.java
@@ -0,0 +1,14 @@
+package ProgramDemo10;
+
+import java.util.Scanner;
+
+public class Stack {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        while(sc.hasNext()){
+            String input=sc.next();
+            while(input.length()!=(input=input.replaceAll("\\(\\)|\\[\\]|\\{\\}","")).length());
+            System.out.println(input.isEmpty());
+        }
+    }
+}
