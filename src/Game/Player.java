b2004f7f2dd91f1765be506f1d9be5a068c7420d
PragnyaParamitaPattanaik <pragnya.pattanaik@gmail.com>
2022-07-23 18:23:43 -0700

    Commit

---
+++ src/Game/Player.java
@@ -0,0 +1,15 @@
+package Game;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Player {
+    String name;
+    int score;
+
+    Player(String name, int score){
+        this.name = name;
+        this.score = score;
+    }
+}
+
